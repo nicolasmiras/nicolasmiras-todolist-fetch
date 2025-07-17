e368a064f2efedada0f688d05986589d6ffea77c
Nico Miras <73142724+nicolasmiras@users.noreply.github.com>
2025-07-17 01:00:40 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("");
+	const [tasks, setTasks] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+	// 
+	const handleInputChange = (event) => {
+		setInputValue(event.target.value);
+	};
+
+	// agregao una tarea cuando aprieto enter
+	const handleKeyDown = (event) => {
+		if (event.key !== "Enter" || inputValue.trim() === "") return;
+		setTasks([...tasks, inputValue]);
+		setInputValue("");
+	};
+
+	// Elimino la tarea
+	const deleteTask = (index) => {
+		setTasks(tasks.filter((_, i) => i !== index));
+	};
+
+	return (
+		<div>
+			<h1>Lista de Pendientes</h1>
+			<input
+				type="text"
+				className="form-control"
+				placeholder="¿Qué Tienes Pendiente?"
+				value={inputValue}
+				onChange={handleInputChange}
+				onKeyDown={handleKeyDown}
+			/>
+
+			<ul>
+				{tasks.map((task, index) => (
+					<li
+						key={index}>
+						{task}
+						<button
+							onClick={() => deleteTask(index)}>
+						</button>
+					</li>
+				))}
+			</ul>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				{tasks.length} {tasks.length === 1 ? "item" : "items"} por hacer
 			</p>
 		</div>
 	);
